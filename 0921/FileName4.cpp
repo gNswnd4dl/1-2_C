8a5845b0e8d8ef77c13bdc1ef8d173bf430e9a83
2unhy2 <144293456+2unhy2@users.noreply.github.com>
2023-09-21 11:55:42 +0900

    Create FileName4.cpp

---
+++ 0921/FileName4.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+
+int main(void) {
+
+	double x = 0;
+	double y = 0;
+
+	int count = 0, circle = 0;
+
+	int percent = 0;
+	double pi;
+
+	srand(time(NULL));
+
+	while (count < 100000) {
+		x = (double)rand() / (double)RAND_MAX;
+		y = (double)rand() / (double)RAND_MAX;
+		count++;
+
+		if ((x * x) + (y * y) <= 1) {
+			circle++;
+		}
+
+
+		pi = 4.0 * circle / (count + 1);
+		percent = count / 1000;
+
+		printf("%d%%진행..원주율 : %lf\n", percent, pi);
+	}
+	return 0;
+}
